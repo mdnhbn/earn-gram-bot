816d69a51f9488c47ecfca446a4a0c6d972887bb
Md Nazmul Hassan Bhuiyan <38237787+mdnhbn@users.noreply.github.com>
2026-03-15 01:33:00 +0300

    Add files via upload

--- components/ActiveTask.tsx
+++ components/ActiveTask.tsx
@@ -115,7 +115,7 @@ const ActiveTask: React.FC<ActiveTaskProps> = ({ task, onClaim, onCancel, isPaus
           <iframe 
             src={playerUrl}
             className="w-full h-full border-none"
-            allow="autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+            allow="autoplay; encrypted-media; fullscreen; picture-in-picture; gyroscope; accelerometer"
             sandbox="allow-forms allow-scripts allow-same-origin allow-presentation"
             title="Secure Player"
             id="task-frame"
